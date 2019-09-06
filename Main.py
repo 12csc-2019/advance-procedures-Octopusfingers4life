f75deb8789eadbe5aa529513a6b0cf312707dc90
Octopusfingers4life <48966338+Octopusfingers4life@users.noreply.github.com>
2019-09-06 12:26:09 +1200

    End of period

--- Main.py
+++ Main.py
@@ -1,33 +1,36 @@
-#importing required modules
-import time
-from graphics import *
+from tkinter import *
 
-win = GraphWin('Music player', 800, 400) # setting up the graphics window
-defaultPoint = Point(0, 0) #creating the default point for my items
+snd = Sound()
 
-#making the window that will hold the music selector
-selector = Rectangle(Point(350, 399), defaultPoint)
-selector.setOutline('black')
+class musicPlayer(Frame):
+    def __init__(self):
+        super().__init__()
 
-selector.draw(win)
+        self.uiMaker()
+    
+    def uiMaker(self):
+        self.master.title("Music player")
+        self.pack(fill=BOTH, expand=1)
+        
+        canvas = Canvas(self)
+        selectSong = canvas.create_rectangle(500, 5, 1.5, 497)
+        musicInfo = canvas.create_rectangle(500, 5, 997, 370)
+        musicControl = canvas.create_rectangle(500, 370, 997, 497)
+        canvas.pack(fill=BOTH, expand=1)
 
-#making the window that will hold the music control
-musicControl = Rectangle(Point(449, 120), defaultPoint)
-musicControl.move(350, 279)
-musicControl.setOutline('black')
-musicControl.draw(win)
+        playMusic = Button(text="play music",bg = 'black', fg = 'white')
+        playMusic.pack()
+        playMusic.place(x = 250, y = 248.5)
 
-#making the window that will hold the info on music
-musicInfo = Rectangle(Point(449, 279), defaultPoint)
-musicInfo.move(350, 0)
-musicInfo.setOutline('black')
-musicInfo.draw(win)
+def mainWindow():
+    main = Tk()
+    ex = musicPlayer()
+    main.geometry('1000x500')
+    main.mainloop()
 
-selectMessage = Text(Point(0, 0), 'Click here to play a song')
-selectMessage.move(180, 190)
-selectMessage.draw(win)
 
-testMessage = Text(Point(0, 0), 'Place holder for the ')
 
-win.getMouse()
-win.close()
+if __name__ == '__main__':
+    mainWindow()
+
+
