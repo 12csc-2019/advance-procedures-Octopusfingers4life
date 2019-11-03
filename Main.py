74dd252e93e583cfc469966859cc1300f5ebbfb0
Octopusfingers4life <48966338+Octopusfingers4life@users.noreply.github.com>
2019-11-04 11:57:19 +1300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,72 +1,131 @@
+#importing tkinter so that i can create the GUI
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+#importing vlc so that i can play the music
+import vlc
 
-    
-from pydub import AudioSegment
-from pydub.playback import play
+#creating global variables so that i can call them across functions
+buttonPos = 5
+fileDir = 'default'
+song = 'default'
 
-
-
+#creating the main class for the music player
 class musicPlayer(Frame):
+    
 
     
     def __init__(self):
         super().__init__()
 
         self.uiMaker()
+
+    
     
     def uiMaker(self):
-        def selectMusic():
-             songName = askopenfilename(filetypes=[("Music files", "*.mp3")])
-             songDir = AudioSegment.from_mp3("test_sound.mp3")
-             play(songDir)
-
+            
+                   
+        #creating the command for the exit button
         def programQuit():
             quit()
 
+        #crating the command for the select song
         def addSong():
+            global buttonPos
+            global fileDir
+            #opening a file explorer to get the user to select a song
             fileDir = askopenfilename()
+            #splitting the file directory name at every / and storing it in a list
             fileSplit = fileDir.rsplit('/')
+            #getting the length of the 'file dir' list 
             fileLength = len(fileSplit)
+            #subtracting one of file length to get the file name
             fileLength = fileLength - 1
+            #storing the file name in a variable
             songName = fileSplit[fileLength]
-            savedSong = Button(text = songName, bg = 'black', fg = 'white')
+            #creating, drawing and moveing the button that displays the file name
+            savedSong = Button(text = songName, bg = 'black', fg = 'white', command = startSong)
             savedSong.pack()
+            savedSong.place(x = 200, y = buttonPos)
+            #updating buttonPos to stop button overlaping
+            buttonPos = buttonPos + 23
+            
 
+        #creating the function that starts the song
+        def startSong():
+            #calling global variables
+            global fileDir
+            global song
+            #setting the media player variable to the song directory
+            song = vlc.MediaPlayer(fileDir)
+            #starting the song
+            song.play()
 
-              
+        #creating the function the pauses the song
+        def pauseMusic(): 
+            #calling the global variable
+            global song  
+            #pausing the song
+            song.pause()
+            
+
+        def playMusic():
+            #calling the global vars
+            global song
+            #playing the song
+            song.play()
+
+        #deffining the text and command of the exit button
+        exitButton = Button(text='X', bg = 'black', fg = 'white', command = programQuit)
+        #drawing the exit button
+        exitButton.pack()
+        #moving the exit button to the correct position
+        exitButton.place(x = 980, y = 5)
+
+        #deffining the text and command of the select song button
+        selectSong  = Button(text='Add a song', bg = 'black', fg = 'white', command = addSong)
+        #drawing the select song button
+        selectSong.pack()
+        #moving the select song button to the correct position
+        selectSong.place(x = 2, y = 5)
+
+        #deffining the text and command of the pause button
+        pauseButton = Button(text='||', bg = 'black', fg = 'white', command = pauseMusic)
+        #drawing the pause button
+        pauseButton.pack()
+        #moving the exit button to the pause position
+        pauseButton.place(x = 775, y = 470)
+
+        #deffining the text and command of the select play button
+        playButton = Button(text = "▶", bg = 'black', fg = 'white', command = song)
+        #drawing the play button
+        playButton.pack()
+        #moving the play button to the correct position
+        playButton.place(x = 750, y = 470)
+
+        #setting the GUI name 
         self.master.title("Music player")
         self.pack(fill=BOTH, expand=1)
         
+        #drawing the boxes on to the GUI
         canvas = Canvas(self)
         songPanel = canvas.create_rectangle(500, 5, 1.5, 497)
         musicInfo = canvas.create_rectangle(500, 5, 997, 370)
         musicControl = canvas.create_rectangle(500, 370, 997, 497)
         canvas.pack(fill=BOTH, expand=1)
 
-        playMusic = Button(text="play music",bg = 'black', fg = 'white', command = selectMusic)
-        playMusic.pack()
-        playMusic.place(x = 250, y = 248.5)
-
-        exitButton = Button(text='X', bg = 'black', fg = 'white', command = programQuit)
-        exitButton.pack()
-        exitButton.place(x = 980, y = 5)
-
-        selectSong  = Button(text='Add a song', bg = 'black', fg = 'white', command = addSong)
-        selectSong.pack()
-        selectSong.place(x = 2, y = 5)
-
+        
 
 
 
 def mainWindow():
     main = Tk()
     ex = musicPlayer()
+    #setting the size of the window
     main.geometry('1000x500')
     main.mainloop()
 
 
-
+#starting the loop
 if __name__ == '__main__':
     mainWindow()
 
---
+++ tests.py
@@ -0,0 +1,2 @@
+import sys
+sys.exec_prefix
